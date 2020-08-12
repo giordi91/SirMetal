6c90fa4120b428e9de6e83e05e924f09dc26686a
Marco Giordano <marco.giordano.work@gmail.com>
2020-08-12 07:54:11 +0100

    basic dense tree that will be used for hierarchy

--- SirMetalLib/SirMetalLib/core/memory/denseTree.h
+++ SirMetalLib/SirMetalLib/core/memory/denseTree.h
@@ -2,43 +2,149 @@
 
 #include <vector>
 #include <cstdint>
+#import <queue>
+#import <unordered_set>
+#import <stack>
 
 namespace SirMetal {
     struct DenseTreeNode {
+        static constexpr uint32_t PARENT_NULL = std::numeric_limits<uint32_t>::max();
         uint32_t id;
         uint32_t index;
-        //Change this for random allocator handle
+        uint32_t parentIndex = PARENT_NULL;
+        //Change this for random allocator handle, or anything else.
+        //anything is better than a vector here
         std::vector<uint32_t> children;
     };
 
     class DenseTree {
-        void initialize(uint32_t capacity) {
-            m_nodes.reserve(capacity);
+    public:
+        void initialize(uint32_t initialSize) {
+            m_nodes.reserve(initialSize);
         }
 
-        DenseTreeNode createNode() {
-            if (m_allocatedNodes >= m_nodes.size()) {grow();}
-            m_nodes[m_allocatedNodes] = {};
-            return m_nodes[m_allocatedNodes];
+        DenseTreeNode &createNode(DenseTreeNode &parent, uint32_t id) {
+            DenseTreeNode &node = createNode(id);
+            parentNode(parent, node);
+            return node;
         };
 
-        void parentNode(uint32_t parentId, uint32_t childrenId) {
-            assert(parentId < m_nodes.size());
-            assert(childrenId < m_nodes.size());
+        DenseTreeNode &createRoot(uint32_t id) {
+            DenseTreeNode &root = createNode(id);
+            m_root = &root;
+            return root;
+        };
 
-            DenseTreeNode& parent = m_nodes[parentId];
-            parent.children.push_back(childrenId);
+
+        void parentNode(DenseTreeNode &parent, DenseTreeNode &children) {
+            assert(parent.index < m_nodes.size());
+            assert(children.index < m_nodes.size());
+
+            parent.children.push_back(children.index);
+
+            if (children.parentIndex != DenseTreeNode::PARENT_NULL) {
+                assert(children.parentIndex < m_nodes.size());
+                DenseTreeNode &childrenParent = m_nodes[children.parentIndex];
+                unparentNode(childrenParent, children);
+            }
+
+            children.parentIndex = parent.index;
+            m_isSorted = false;
+        }
+
+        bool isDirectParent(const DenseTreeNode &parent, const DenseTreeNode &child) const {
+            return parent.index == child.parentIndex;
+        }
+
+        bool isDirectChild(const DenseTreeNode &parent, const DenseTreeNode &child) const {
+            size_t count = parent.children.size();
+            bool result = false;
+            for (size_t i = 0; i < count; ++i) {
+                result |= parent.children[i] == child.index;
+            }
+            return result;
+        }
+
+        std::vector<DenseTreeNode> &getNodes() {
+            return m_nodes;
+        };
+
+        DenseTreeNode *getRoot() {
+            return m_root;
+        }
+
+        //this can be done externally, for now I will be doing it as member class
+        //to make it simpler and more efficient
+        void depthFirstSort() {
+            //we simply rewrite the indices then ask the std::sort to sort based
+            //on that
+            if (m_isSorted) {return;}
+
+            m_sortIndices.push(m_root->index);
+            m_visitedNodes.clear();
+            int counter = 0;
+            while (!m_sortIndices.empty()) {
+                const uint32_t idx = m_sortIndices.top();
+                DenseTreeNode &node = m_nodes[idx];
+                node.index = counter++;
+                m_sortIndices.pop();
+                m_visitedNodes.insert(idx);
+                for (uint32_t child : node.children) {
+                    if (m_visitedNodes.find(child) == m_visitedNodes.end()) {
+                        m_sortIndices.push(child);
+                    }
+                }
+            }
+
+            std::sort(m_nodes.begin(), m_nodes.end(),
+                    [](const DenseTreeNode &lhs, const DenseTreeNode &rhs) {
+                        return lhs.index < rhs.index;
+                    });
+            m_isSorted = true;
+
+        }
+
+        bool isSorted() const {
+            return m_isSorted;
         }
 
     private:
-        void grow() {
-            size_t currSize = m_nodes.size() == 0 ? 1 : m_nodes.size();
-            m_nodes.resize(currSize * 2);
+        DenseTreeNode &createNode(uint32_t id) {
+            m_nodes.emplace_back(DenseTreeNode{id, static_cast<uint32_t>(m_nodes.size())});
+            DenseTreeNode &node = m_nodes[m_nodes.size() - 1];
+            m_isSorted = false;
+            return node;
+
         };
 
+        void unparentNode(DenseTreeNode &parent, DenseTreeNode &child) {
+            int index = -1;
+            size_t count = parent.children.size();
+            for(size_t i=0; i < count; ++i)
+            {
+                if(parent.children[i] == child.index)
+                {
+                    index = i;
+                }
+            }
+            assert(index != -1);
+            if(index != -1) {
+                //patching element from last
+                parent.children[index] = parent.children[count-1];
+                parent.children.pop_back();
+                child.parentIndex = DenseTreeNode::PARENT_NULL;
+                m_isSorted = false;
+            }
+
+        }
+
     private:
-        uint32_t m_allocatedNodes = 0;
+        bool m_isSorted = true;
         std::vector<DenseTreeNode> m_nodes;
+        DenseTreeNode *m_root = nullptr;
+        std::stack<uint32_t> m_sortIndices;
+        std::unordered_set<uint32_t> m_visitedNodes;
     };
+
 
 }
--- SirMetalTests/SirMetalTests/denseTreeTests.mm
+++ SirMetalTests/SirMetalTests/denseTreeTests.mm
@@ -1,13 +1,88 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "SirMetalLib/core/memory/denseTree.h"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+
+TEST_CASE( "dense tree create node", "[memory]" ) {
+    SirMetal::DenseTree tree;
+    tree.initialize(16);
+    SirMetal::DenseTreeNode& node = tree.createRoot(0);
+
+    REQUIRE( node.id == 0);
+    REQUIRE( node.index == 0);
+
+    SirMetal::DenseTreeNode& node2 = tree.createNode(node,2);
+
+    REQUIRE( node2.id == 2);
+    REQUIRE( node2.index == 1);
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE( "dense tree basic node parenting", "[memory]" ) {
+    SirMetal::DenseTree tree;
+    tree.initialize(16);
+    SirMetal::DenseTreeNode& node1 = tree.createRoot(1);
+    SirMetal::DenseTreeNode& node2 = tree.createNode(node1,2);
+
+    REQUIRE(node2.parentIndex == node1.index);
+    REQUIRE(node1.children.size() == 1);
+    REQUIRE(node1.children[0] == node2.index);
+    REQUIRE(tree.isDirectParent(node1,node2));
+    REQUIRE(tree.isDirectChild(node1,node2));
 }
+
+TEST_CASE( "dense tree DFS sorting", "[memory]" ) {
+
+    //we will make the same tree as
+    //https://en.wikipedia.org/wiki/Depth-first_search#/media/File:Depth-First-Search.gif
+    SirMetal::DenseTree tree;
+    tree.initialize(16);
+    SirMetal::DenseTreeNode& node1 = tree.createRoot(1);
+    SirMetal::DenseTreeNode& node2 = tree.createNode(node1,2);
+    SirMetal::DenseTreeNode& node5 = tree.createNode(node1,5);
+    SirMetal::DenseTreeNode& node9 = tree.createNode(node1,9);
+
+    SirMetal::DenseTreeNode& node3 = tree.createNode(node2,3);
+    SirMetal::DenseTreeNode& node6 = tree.createNode(node5,6);
+    SirMetal::DenseTreeNode& node8 = tree.createNode(node5,8);
+    SirMetal::DenseTreeNode& node10 = tree.createNode(node9,10);
+
+    tree.createNode(node3,4);
+    tree.createNode(node6,7);
+
+    std::vector<uint32_t> initialIds = {1,2,5,9,3,6,8,10,4,7};
+    auto nodes= tree.getNodes();
+    size_t count = nodes.size();
+    REQUIRE(count == initialIds.size());
+    for(size_t i =0; i <count;++i)
+    {
+        REQUIRE(nodes[i].id == initialIds[i]);
+    }
+
+    tree.depthFirstSort();
+    nodes= tree.getNodes();
+    std::vector<uint32_t> expectedIds= {1,9,10,5,8,6,7,2,3,4};
+    count = nodes.size();
+    REQUIRE(count == expectedIds.size());
+    for(size_t i =0; i <count;++i)
+    {
+        REQUIRE(nodes[i].id == expectedIds[i]);
+    }
+}
+
+TEST_CASE( "dense tree basic node parenting with prvious parent", "[memory]" ) {
+    SirMetal::DenseTree tree;
+    tree.initialize(16);
+    SirMetal::DenseTreeNode &node1 = tree.createRoot(1);
+    SirMetal::DenseTreeNode &node2 = tree.createNode(node1, 2);
+    SirMetal::DenseTreeNode &node3 = tree.createNode(node1, 2);
+
+    tree.parentNode(node2,node3);
+
+    REQUIRE(tree.isDirectChild(node1,node2));
+    REQUIRE(!tree.isDirectChild(node1,node3));
+    REQUIRE(!tree.isDirectParent(node1,node3));
+    REQUIRE(tree.isDirectChild(node2,node3));
+    REQUIRE(tree.isDirectParent(node2,node3));
+}
+
+
